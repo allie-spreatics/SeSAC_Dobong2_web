9e514d3e4ecbf9b23b05c89e6f3e34761ac1404d
jinhyeong830 <shg02387@naver.com>
2025-01-13 14:48:30 +0900

    250113: react ref&lifecycle

--- 22-ref-lifecyle/src/App.jsx
+++ 22-ref-lifecyle/src/App.jsx
@@ -1,8 +1,29 @@
+import LifeCycleClass from "./components/LifecycleClass";
+import LifeCycleFunc from "./components/LifecycleFunc";
+import Container from "./components/practice/Container";
+import FakePost from "./components/practice/FakePost";
+import RealPost from "./components/practice/RealPost";
+import { RefClass1, RefClass2 } from "./components/RefClass";
+import { RefFunc1, RefFunc2 } from "./components/RefFunc";
+import Test from "./components/Test";
+
 function App() {
   return (
     <div>
-      <h1>다음시간부터</h1>
-      <h2>ref를 해보겠습니다!</h2>
+      {/* ref */}
+      {/* <RefClass1 />
+      <RefClass2 /> 
+      <RefFunc1 />
+      <RefFunc2 />
+      */}
+      {/* lifecycle */}
+      <LifeCycleClass />
+      <LifeCycleFunc />
+      {/* <Test /> */}
+      <Container>
+        {/* <FakePost /> */}
+        <RealPost />
+      </Container>
     </div>
   );
 }

16ea3c32fcdb5460dff69b9e6a7ead9e13f53eed
jinhyeong830 <shg02387@naver.com>
2025-01-31 15:35:32 +0900

    250131: react ts 연습

--- 28_typescript/test.js
+++ 28_typescript/test.js
@@ -22,3 +22,14 @@ jsPrint(1, 2, "3", 4, 5, 6);
 function addSomething(a, b) {
   return a + b; // number, string
 }
+
+function sum(...nums) {
+  let sum = 0;
+  for (let num of nums) {
+    sum += num;
+  }
+
+  return sum;
+}
+
+console.log(sum(1, 2, 3, 4, 5));
